52658044e6f2d10a1da4a43624acc097f8773a62
Simon Tech <contato.simon.tech@gmail.com>
2025-07-28 11:06:04 -0300

    delete btn

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -26,23 +26,48 @@ if (clientsList) {
         .then(data => {
             data.forEach(client => {
                 const li = document.createElement("li");
-
+                
                 li.className = "list-group-item d-flex justify-content-between align-items-center";
-
+                
                 li.innerHTML = `
-                    <span class="col-3 text-break"><a href="client.html?client_id=${client.client_id}" class="text-decoration-none"> ${client.first_name} ${client.last_name}</a></span>
-                    <span class="col-2">${client.gender}</span>
-                    <span class="col-3 text-break p-2">${client.social_media}</span>
-                    <span class="col-3 text-break">${client.phone}</span>
+                <span class="col-3 text-break"><a href="client.html?client_id=${client.client_id}" class="text-decoration-none"> ${client.first_name} ${client.last_name}</a></span>
+                <span class="col-2">${client.gender}</span>
+                <span class="col-3 text-break p-2">${client.social_media}</span>
+                <span class="col-3 text-break">${client.phone}</span>
                     <span class="col-1 text-end">
                         <button class="btn btn-sm btn-warning me-1"><i class="fas fa-edit"></i></button>
-                        <button class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i></button>
+                        <button class="btn btn-sm btn-danger delete-btn"><i class="fas fa-trash-alt"></i></button>
                         </span>
-                `;
+                        `;
+                        
+                const deleteBtn = li.querySelector(".delete-btn");
+
+                deleteBtn.addEventListener("click", () => {
+                    if (confirm(`Excluir ${client.first_name}?`)) {
+                        fetch(`${API_URL}/clients/${client.client_id}`, {
+                            method: "DELETE",
+                            headers: {
+                                Authorization: `Bearer ${token}`
+                            }
+                        })
+                        .then(res => {
+                            if(!res.ok) {
+                                throw new Error("Erro ao excluir o cliente.");
+                            }
+                            li.remove();
+                        })
+                        .catch(err => {
+                            alert("Erro ao excluir cliente: " + err.message);
+                        })
+                    }
+                })
+
                 clientsList.appendChild(li);
+
             });
         });
 }
+
 
 function logout() {
     localStorage.removeItem("token");
